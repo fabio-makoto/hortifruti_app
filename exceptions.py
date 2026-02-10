e6ca8970038d6017c542c6297cef1d47758af46a
fabio-makoto <nakasimafabio@gmail.com>
2026-02-10 00:18:30 -0300

    created a file of exceptions
    this file contains the custom exceptions for each error handling

---
+++ exceptions.py
@@ -0,0 +1,24 @@
+class AllFieldsAreRequired(Exception):
+    pass
+
+class DataNotFound(Exception):
+    pass
+
+class LimitExceeded(Exception):
+    pass
+
+class DataAlreadyExists(Exception):
+    pass
+
+class CannotBeZero(Exception):
+    pass
+
+class CannotBeNegative(Exception):
+    pass
+
+class IncorrectValue(Exception):
+    pass
+
+class CannotBeEmpty(Exception):
+    pass
+
